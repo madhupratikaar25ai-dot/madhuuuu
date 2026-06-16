55a0f5b5a8e8c60ce2541551e2f4ec7b3b2b1362
MADHU PRATIKA <madhupratikaar25ai@vetias.ac.in>
2026-06-16 16:10:52 +0530

    using linked list

--- src/day10.java
+++ src/day10.java
@@ -1,28 +1,47 @@
-import java.util.ArrayList;
+
+/*import java.util.ArrayList;
 public class day10 {
- static void main() {
-     String[] Student= {"Madhu","Supriya","Ram","Rey","P" +
-             "radhish"};
-     System.out.println(Student.length);
-     ArrayList<String> list = new ArrayList();
-     list.add("Madhu");
-     list.add("Supriya");
-     list.add("Ram");
-     list.add("Rey");
-     list.add("Pradhish");
-     for(String s: list) {
-         System.out.print(s);
-     }
-         list.remove("Ram");
-         System.out.println(list.getFirst());
-         System.out.println(list.getLast());
-     System.out.println();
-     for(String s: list) {
-         System.out.println(s);
-     }
-     System.out.println();
-         System.out.println(list);
-     }
- }
+    static void main() {
+        String[] Student = {"Madhu", "Supriya", "Ram", "Rey", "Pradhish"};
+        System.out.println(Student.length);
+        ArrayList<String> list = new ArrayList();
+        list.add("Madhu");
+        list.add("Supriya");
+        list.add("Ram");
+        list.add("Rey");
+        list.add("Pradhish");
+        for (String s : list) {
+            System.out.print(s);
+        }
+        list.remove("Ram");
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println();
+        System.out.println(list);
+    }
+}
+*/
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class day10 {
+    static void process(LinkedList<String>list){
+        System.out.println("Removed:" + list.removeFirst());
+    }
+   static void main() {
+        LinkedList<String>list = new LinkedList<>();
+        list.add("Madhu");
+        list.add("Supriya");
+        list.add("Ram");
+        list.add("Rey");
+        list.add("Pradhish");
+        while (list.size()> 0) {
+            process(list);
+        }
+        System.out.println("queue is empty");
+    }
+}
 
 
