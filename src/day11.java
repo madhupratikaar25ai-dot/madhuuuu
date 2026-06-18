4182880052225cf6d115bf68769b4331d71a4f4d
MADHU PRATIKA <madhupratikaar25ai@vetias.ac.in>
2026-06-18 09:50:56 +0530

    using map

--- src/day11.java
+++ src/day11.java
@@ -1,4 +1,7 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/*import java.util.HashSet;
 import java.util.Set;
 public class day11 {
     public static void main(String[] args) {
@@ -12,4 +15,19 @@ public class day11 {
             System.out.println(s);
         }
     }
+}
+*/
+ public class day11{
+      public static void main(String[]args){
+          Map<String,Integer> person = new HashMap<>();
+          person.put("Ram",89);
+          person.put("john",78);
+          person.put("Aravindhan",67);
+          person.put("Ramesh", 90);
+                  System.out.println(person.get("percent"));
+          person.entrySet().forEach(entry ->{
+              System.out.println(entry.getKey() + " : " + entry.getValue());
+          });
+
+      }
 }
